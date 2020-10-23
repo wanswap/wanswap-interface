a644442a427c75fc139af4c5ffe99e5f8f2b0d91
moohooooo <suwichalala@gmail.com>
2020-10-23 20:03:07 +0700

    Update index.tsx

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -22,7 +22,7 @@ const StyledLogo = styled(Logo)<{ size: string }>`
   height: ${({ size }) => size};
   border-radius:50%;
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
-  background:black
+  
 `
 
 export default function CurrencyLogo({
