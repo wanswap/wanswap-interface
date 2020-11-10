b5a76b0bfcf19369f4fcbc44f76170c23e30bd76
moohooooo <suwichalala@gmail.com>
2020-11-10 13:17:40 +0700

    Increase loading size

--- src/components/ModalViews/index.tsx
+++ src/components/ModalViews/index.tsx
@@ -28,7 +28,7 @@ export function LoadingView({ children, onDismiss }: { children: any; onDismiss:
         <CloseIcon onClick={onDismiss} />
       </RowBetween>
       <ConfirmedIcon>
-        <CustomLightSpinner src={Circle} alt="loader" size={'90px'} />
+        <CustomLightSpinner src={Circle} alt="loader" size={'150px'} />
       </ConfirmedIcon>
       <AutoColumn gap="100px" justify={'center'}>
         {children}
