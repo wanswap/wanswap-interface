044d262c1b1fe950e4e3b27ea064fa99839e2b72
Morpheus <morpheus.blockchain@gmail.com>
2020-12-12 14:31:31 +0800

    finish inside wallet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@wanswap/sdk'
 import { AbstractConnector } from '@web3-react-wan/abstract-connector'
 
-import { injected, wanwallet, 
-  // walletconnect 
-} from '../connectors'
+import { injected, wanwallet } from '../connectors'
 
 export const ROUTER_ADDRESS = '0xeA300406FE2eED9CD2bF5c47D01BECa8Ad294Ec1'
 
@@ -118,16 +116,7 @@ const SUPPORTED_WALLETS_CHROME: { [key: string]: WalletInfo } = {
     description: 'Easy-to-use browser extension.',
     href: null,
     color: '#E8831D'
-  },
-  // WALLET_CONNECT: {
-  //   connector: walletconnect,
-  //   name: 'WalletConnect',
-  //   iconName: 'walletConnectIcon.svg',
-  //   description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
-  //   href: null,
-  //   color: '#4196FC',
-  //   mobile: true
-  // },
+  }
 }
 
 const SUPPORTED_WALLETS_IN_WALLET: { [key: string]: WalletInfo } = {
