13b64e45ce3f47b9c1e644e4316455b28643edd5
Morpheus <morpheus.blockchain@gmail.com>
2020-12-30 10:59:30 +0800

    update for support andriod7

--- src/index.tsx
+++ src/index.tsx
@@ -41,19 +41,6 @@ window.addEventListener('error', error => {
   })
 })
 
-// Add for support Andriod7
-if (!Object.entries) {
-  Object.entries = function( obj: any ){
-    var ownProps = Object.keys( obj ),
-        i = ownProps.length,
-        resArray = new Array(i); // preallocate the Array
-    while (i--)
-      resArray[i] = [ownProps[i], obj[ownProps[i]]];
-
-    return resArray;
-  };
-}
-
 function Updaters() {
   return (
     <>
