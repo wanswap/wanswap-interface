c3618a9851fb52e568f93fb022ce83502a22560d
Morpheus <morpheus.blockchain@gmail.com>
2021-01-04 13:29:41 +0800

    update menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -52,7 +52,7 @@ const StyledMenu = styled.div`
 `
 
 const MenuFlyout = styled.span`
-  min-width: 8.125rem;
+  min-width: 9.125rem;
   background-color: ${({ theme }) => theme.bg3};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
@@ -116,7 +116,7 @@ export default function Menu() {
             Docs
           </MenuItem>
           <MenuItem id="link" href="https://github.com/wanswap/wanswap-interface/discussions">
-            <BookOpen size={14} />
+            <MessageCircle size={14} />
             Discussions
           </MenuItem>
           <MenuItem id="link" href="https://github.com/wanswap">
