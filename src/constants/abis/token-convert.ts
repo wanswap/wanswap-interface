261bba8cc1cf22e5f359f94520a617ff1c36b452
Morpheus <morpheus.blockchain@gmail.com>
2021-03-04 16:54:33 +0800

    update wanBTC swap wanOBTC

--- src/constants/abis/token-convert.ts
+++ src/constants/abis/token-convert.ts
@@ -12,21 +12,24 @@ export const TOKEN_CONVERT_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.KOVAN]: '0xAfa6C39FDf16446078aEeEE53E4c4843811570A1'
 }
 
-const token0 = "0x89a3e1494bc3db81dadc893ded7476d33d47dcbd";
-const token1 = "0x07fdb4e8f8e420d021b9abeb2b1f6dce150ef77c";
+const token0Testnet = "0x89a3e1494bc3db81dadc893ded7476d33d47dcbd";
+const token1Testnet = "0x07fdb4e8f8e420d021b9abeb2b1f6dce150ef77c";
+
+const token0Mainnet = "0x89a3e1494bc3db81dadc893ded7476d33d47dcbd";
+const token1Mainnet = "0x07fdb4e8f8e420d021b9abeb2b1f6dce150ef77c";
 
 export const CONVERT_TOKEN0 : { [chainId in ChainId]: Token } = {
-  [ChainId.MAINNET]: new Token(ChainId.MAINNET, token0, 18, 'wanOBTC', 'wanOBTC'),
-  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, token0, 18, 'wanOBTC', 'wanOBTC'),
-  [ChainId.ROPSTEN]: new Token(ChainId.ROPSTEN, token0, 18, 'wanOBTC', 'wanOBTC'),
-  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, token0, 18, 'wanOBTC', 'wanOBTC'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, token0, 18, 'wanOBTC', 'wanOBTC')
+  [ChainId.MAINNET]: new Token(ChainId.MAINNET, token0Mainnet, 18, 'wanOBTC', 'wanOBTC'),
+  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, token0Testnet, 18, 'wanOBTC', 'wanOBTC'),
+  [ChainId.ROPSTEN]: new Token(ChainId.ROPSTEN, token0Testnet, 18, 'wanOBTC', 'wanOBTC'),
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, token0Testnet, 18, 'wanOBTC', 'wanOBTC'),
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, token0Testnet, 18, 'wanOBTC', 'wanOBTC')
 }
 
 export const CONVERT_TOKEN1 : { [chainId in ChainId]: Token } = {
-  [ChainId.MAINNET]: new Token(ChainId.MAINNET, token1, 18, 'wanBTC', 'wanBTC'),
-  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, token1, 18, 'wanBTC', 'wanBTC'),
-  [ChainId.ROPSTEN]: new Token(ChainId.ROPSTEN, token1, 18, 'wanBTC', 'wanBTC'),
-  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, token1, 18, 'wanBTC', 'wanBTC'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, token1, 18, 'wanBTC', 'wanBTC')
+  [ChainId.MAINNET]: new Token(ChainId.MAINNET, token1Mainnet, 18, 'wanBTC', 'wanBTC'),
+  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, token1Testnet, 18, 'wanBTC', 'wanBTC'),
+  [ChainId.ROPSTEN]: new Token(ChainId.ROPSTEN, token1Testnet, 18, 'wanBTC', 'wanBTC'),
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, token1Testnet, 18, 'wanBTC', 'wanBTC'),
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, token1Testnet, 18, 'wanBTC', 'wanBTC')
 }
