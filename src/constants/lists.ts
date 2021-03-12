b83dc8bba835f5189a9f88e1453bf98dc4a1b46f
Morpheus <morpheus.blockchain@gmail.com>
2021-03-12 17:33:22 +0800

    update base url of token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,8 @@
 // the Uniswap Default token list lives here
 // export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-export const DEFAULT_TOKEN_LIST_URL = 'https://wanswap.finance/wanswap.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = window.location.origin + '/wanswap.tokenlist.json'
+
+console.log('DEFAULT_TOKEN_LIST_URL', DEFAULT_TOKEN_LIST_URL);
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
