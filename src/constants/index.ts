e8a303bea2dd53a0e747638f2179904613bf9eff
lolieatapple <lolieatapple@gmail.com>
2021-03-15 10:46:40 +0800

    update mobile support

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -125,7 +125,8 @@ const SUPPORTED_WALLETS_CHROME: { [key: string]: WalletInfo } = {
     iconName: 'metamask.png',
     description: 'Easy-to-use browser extension.',
     href: null,
-    color: '#E8831D'
+    color: '#E8831D',
+    mobile: true
   },
   // WALLET_CONNECT: {
   //   connector: walletconnect,
