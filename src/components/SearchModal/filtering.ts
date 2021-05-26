61939fe167479959c7314e82d068bceb331bf461
T Jakubek <t.jakubek@hotmail.com>
2021-05-26 21:43:48 +0200

    Allow for token search to match within token name

--- src/components/SearchModal/filtering.ts
+++ src/components/SearchModal/filtering.ts
@@ -1,6 +1,12 @@
 import { isAddress } from '../../utils'
 import { Token } from '@wanswap/sdk'
 
+const prepareTermForSearch = (term: string) =>
+  term
+    .toLowerCase()
+    .split(/\s+/)
+    .filter(s => s.length > 0)
+
 export function filterTokens(tokens: Token[], search: string): Token[] {
   if (search.length === 0) return tokens
 
@@ -10,22 +16,20 @@ export function filterTokens(tokens: Token[], search: string): Token[] {
     return tokens.filter(token => token.address === searchingAddress)
   }
 
-  const lowerSearchParts = search
-    .toLowerCase()
-    .split(/\s+/)
-    .filter(s => s.length > 0)
+  //will remove comments before merge, just wondering what this is meant to do?
+  //It doesn't seem like you can write whitespace characters into the search bar so the split doesn't make much sense to me
+  const lowerSearchParts = prepareTermForSearch(search)
 
   if (lowerSearchParts.length === 0) {
     return tokens
   }
 
   const matchesSearch = (s: string): boolean => {
-    const sParts = s
-      .toLowerCase()
-      .split(/\s+/)
-      .filter(s => s.length > 0)
+    //likewise, I dont think you're symbols or names will have whitespace characters in them
+    const sParts = prepareTermForSearch(s)
 
-    return lowerSearchParts.every(p => p.length === 0 || sParts.some(sp => sp.startsWith(p) || sp.endsWith(p)))
+    // return lowerSearchParts.every(p => p.length === 0 || sParts.some(sp => sp.startsWith(p) || sp.endsWith(p)))
+    return lowerSearchParts.every(p => p.length === 0 || sParts.some(sp => sp.includes(p)))
   }
 
   return tokens.filter(token => {
