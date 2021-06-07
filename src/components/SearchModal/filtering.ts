3ead3d89dc622c713681f462cd5afe6db8752360
T Jakubek <t.jakubek@hotmail.com>
2021-06-07 09:07:07 +0200

    reverting comments and function isolation

--- src/components/SearchModal/filtering.ts
+++ src/components/SearchModal/filtering.ts
@@ -1,12 +1,6 @@
 import { isAddress } from '../../utils'
 import { Token } from '@wanswap/sdk'
 
-const prepareTermForSearch = (term: string) =>
-  term
-    .toLowerCase()
-    .split(/\s+/)
-    .filter(s => s.length > 0)
-
 export function filterTokens(tokens: Token[], search: string): Token[] {
   if (search.length === 0) return tokens
 
@@ -16,17 +10,20 @@ export function filterTokens(tokens: Token[], search: string): Token[] {
     return tokens.filter(token => token.address === searchingAddress)
   }
 
-  //will remove comments before merge, just wondering what this is meant to do?
-  //It doesn't seem like you can write whitespace characters into the search bar so the split doesn't make much sense to me
-  const lowerSearchParts = prepareTermForSearch(search)
+  const lowerSearchParts = search
+    .toLowerCase()
+    .split(/\s+/)
+    .filter(s => s.length > 0)
 
   if (lowerSearchParts.length === 0) {
     return tokens
   }
 
   const matchesSearch = (s: string): boolean => {
-    //likewise, I dont think you're symbols or names will have whitespace characters in them
-    const sParts = prepareTermForSearch(s)
+    const sParts = s
+      .toLowerCase()
+      .split(/\s+/)
+      .filter(s => s.length > 0)
 
     return lowerSearchParts.every(p => p.length === 0 || sParts.some(sp => sp.includes(p)))
   }
