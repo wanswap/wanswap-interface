91de0f4e2cf9b1a1be25d4a21d820e53255f8bcc
Morpheus <morpheus.blockchain@gmail.com>
2022-01-28 17:17:18 +0800

    update moonriver addr

--- src/components/Header/UniBalanceContent.tsx
+++ src/components/Header/UniBalanceContent.tsx
@@ -132,8 +132,8 @@ export default function UniBalanceContent({ setShowUniBalanceModal }: { setShowU
               <TYPE.white color="white"><a href="https://www.wanscan.org/token/0x8b9f9f4aa70b1b0d586be8adfb19c1ac38e05e9a">0x8b9f...5e9a</a></TYPE.white>
             </RowBetween>
             <RowBetween>
-              <TYPE.white color="white">WASP Token Address(ERC20):</TYPE.white>
-              <TYPE.white color="white"><a href="https://etherscan.io/token/0xef5c6a88710a3c857105058f947d249bc490909d">0xef5c...909d</a></TYPE.white>
+              <TYPE.white color="white">WASP Token Address(ERC20 on Moonriver):</TYPE.white>
+              <TYPE.white color="white"><a href="https://moonriver.moonscan.io/token/0xffef2639b2ee39f9c284d0107e567dd2f7b20613">0xffef...0613</a></TYPE.white>
             </RowBetween>
             {uni && uni.chainId === ChainId.MAINNET ? (
               <ExternalLink href={`https://info.wanswap.finance/token/0x8b9f9f4aa70b1b0d586be8adfb19c1ac38e05e9a`}>View WASP Statistics</ExternalLink>
